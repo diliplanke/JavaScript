f4811c23781feced22edfb8986c1168260856721
dilip_dilz <dilip_dilz@yahoo.com>
2022-02-22 05:47:28 -0800

    adding chunk fuzzbuss and maxchar

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -1,13 +1,29 @@
 // --- Directions
 // Given an array and chunk size, divide the array into many subarrays
 // where each subarray is of length size
-// --- Examples
+// --- Example  s
 // chunk([1, 2, 3, 4], 2) --> [[ 1, 2], [3, 4]]
 // chunk([1, 2, 3, 4, 5], 2) --> [[ 1, 2], [3, 4], [5]]
-// chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[ 1, 2, 3], [4, 5, 6], [7, 8]]
+// chunk([1, 2, 3, 4, 5, 6, 7,  8], 3) --> [[ 1, 2, 3], [4, 5, 6], [7, 8]]
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
-
+function chunk(array, size) {
+  let chunkArray = [];
+  let dummy = [];
+  console.log(array.length)
+  for (let i=0; i < array.length; i++){
+    if (i != 0 && ( i + 1) % size == 0){
+       dummy.push(array[i]);
+       chunkArray.push(dummy.values());
+        console.log(chunkArray);
+       dummy.length = 0;
+    }
+    else {
+      dummy.push(array[i]);
+    }
+  }
+  console.log(chunkArray);
+}
+chunk([1,2,3,4,5,6],3)
 module.exports = chunk;
--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,25 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const chars = {};
+  const maxchars ={};
+  let highval = 0;
 
+  for ( let char of str ) {
+    chars[char] = chars[char] +1 || 1;
+    if ( parseInt(chars[char]) > highval ){
+       highval = parseInt(chars[char]);
+    }
+  }
+  console.log(highval);
+  for (const [key, value] of Object.entries(chars)){
+          console.log(key, value)
+          if(value === highval){
+            console.log(key)
+            return key
+          }
+  }
+}
+maxChar('Halley Berry')
 module.exports = maxChar;
